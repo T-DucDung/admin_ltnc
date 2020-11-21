aaf8270e89e61d7308916ba21ae391155f5af265
TranDucDung <cbcbvn99@gmail.com>
2020-11-21 15:14:37 +0700

    len

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import UserProfile from "views/UserProfile.jsx";
+import TableList from "views/TableList.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/user",
+    name: "User Profile",
+    icon: "pe-7s-user",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Table List",
+    icon: "pe-7s-note2",
+    component: TableList,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
